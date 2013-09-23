cd61c5e1897933b9eed7878002aa2ebc6cb3cc00
Ryan Richard <ryanrichard07@gmail.com>
2013-09-23 13:28:08 -0500

    initial commit, berks, test-kitchen

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "jenkins-rolebook"
+maintainer       "Rackspace, US Inc."
+maintainer_email "ryan.richard@rackspace.com"
+license          "Apache 2.0"
+description      "Installs and configures Jenkins CI server & slaves"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "jenkins"
